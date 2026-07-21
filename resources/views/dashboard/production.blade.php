8f1695729f6ba0a4d2b42d9f7ca50e33840be02d
aphynt <ahmadfadillah502@gmail.com>
2026-07-21 16:51:03 +0800

    update

--- resources/views/dashboard/production.blade.php
+++ resources/views/dashboard/production.blade.php
@@ -2,13 +2,40 @@
 @include('layout.sidebar')
 @include('layout.header')
 
-<div class="card-body p-0">
+<style>
+    #kt_content {
+        padding: 0 !important;
+    }
 
-    <iframe
-        src="{{ config('ssrs.url') . '/ReportServer?/Portal/Dashboard%20Production&rs:Command=Render&rs:Embed=true' }}"
-        width="100%" height="900" frameborder="0" style="border:none;">
-    </iframe>
+    #kt_post {
+        padding: 0 !important;
+    }
 
+    #kt_content_container {
+        padding: 0 !important;
+        margin: 0 !important;
+        height: calc(100vh - 70px);
+    }
+
+    .ssrs-frame {
+        width: 100%;
+        height: 100%;
+        border: none;
+        display: block;
+    }
+</style>
+
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <div class="post d-flex flex-column-fluid" id="kt_post">
+        <div id="kt_content_container" class="container-fluid">
+
+            <iframe
+                class="ssrs-frame"
+                src="{{ config('ssrs.url') }}/ReportServer?/Portal/Dashboard%20Production&rs:Command=Render&rs:Embed=true">
+            </iframe>
+
+        </div>
+    </div>
 </div>
 
 @include('layout.footer')
